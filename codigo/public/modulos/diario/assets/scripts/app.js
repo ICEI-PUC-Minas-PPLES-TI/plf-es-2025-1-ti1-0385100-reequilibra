038f4bc9e400536735d59f1397e22d5636fd8cf7
fillmello <fillmello@outlook.com>
2025-05-15 23:20:12 -0300

    Responsively create card based on input

--- codigo/public/modulos/diario/assets/scripts/app.js
+++ codigo/public/modulos/diario/assets/scripts/app.js
@@ -5,14 +5,24 @@ btnSalvarDiario.addEventListener('click', function(event){
     event.preventDefault();
     const tituloDiario = document.getElementById('tituloDiario').value;
     const textoDiario = document.getElementById('textoDiario').value;
+    const date = new Date();
 
-
-    document.getElementById('teste').innerHTML = ` Título ${tituloDiario}, Texto ${textoDiario}, Status ${statusDiario}`;
+    listaDiarios(1, tituloDiario, textoDiario, statusDiario, date.toLocaleDateString('pt-BR'), date.toLocaleTimeString('pt-BR'));
 });
 
 // ============== BOTÕES STATUS ================
 function defStatusDiario(i){
+    //resetar os botões
+    const resetBtn = document.querySelectorAll('#btnStatus1, #btnStatus2, #btnStatus3, #btnStatus4, #btnStatus5');
+    resetBtn.forEach(btn => btn.classList.replace('btn-success','btn-light')); 
+
+
     statusDiario = i;
+    const btnStatus = document.getElementById(`btnStatus${i}`);
+
+    btnStatus.classList.replace('btn-light','btn-success');
+ 
+
 }
 
 
