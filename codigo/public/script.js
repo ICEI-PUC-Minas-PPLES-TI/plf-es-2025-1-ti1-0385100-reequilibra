cc2c1f99e086fd11aa5a38f730c01ff306696d5d
AndersonRodrigues8 <andersonjrleal16@gmail.com>
2025-05-31 13:24:12 -0300

    chore: add inicialização dos scripts

---
+++ codigo/public/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  initThemeToggle();
+  initMobileMenu();
+  initSmoothScroll();
+  initSaibaMais();
+  initHeaderScroll();
+  initIntersectionAnimations();
+  initHeroStatsCounter();
+});
